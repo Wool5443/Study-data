f13f24687298cb1f0be790b61c6ddd235faa3d02
Wool5443 <mihsolodilov2015@gmail.com>
2025-11-26 19:45:32 +0300

    Upd
    Signed-off-by: Wool5443 <mihsolodilov2015@gmail.com>

--- Module_2/Programming/lab4/additional.c
+++ Module_2/Programming/lab4/additional.c
@@ -9,6 +9,8 @@ static void print_string(const char* start, const char* end);
 
 static void solution(const char* string)
 {
+    printf("Naive\n");
+
     size_t len = strlen(string);
 
     for (const char* start = string; start < string + len; start++)
@@ -24,10 +26,69 @@ static void solution(const char* string)
     }
 }
 
+static void solution2(const char* string)
+{
+    printf("Linear\n");
+
+    size_t len = strlen(string);
+    size_t new_len = 2 * len + 3;
+    char* new_string = calloc(new_len + 1, 1);
+
+    constexpr char GUARD = 1;
+
+    new_string[0] = GUARD;
+    new_string[new_len - 2] = '\0';
+    new_string[new_len - 1] = GUARD;
+
+    for (size_t i = 0; i < len; i++)
+    {
+        new_string[2 * i + 1] = '\0';
+        new_string[2 * i + 2] = string[i];
+    }
+
+    size_t* radiuses = calloc(new_len, sizeof(*radiuses));
+
+    for (size_t i = 1; i < new_len - 1; i++)
+    {
+        size_t r = 0;
+
+        while (new_string[i - r] != GUARD && new_string[i + r] != GUARD && new_string[i - r] == new_string[i + r])
+        {
+            r++;
+        }
+        r--;
+        radiuses[i] = r;
+    }
+
+    for (size_t i = 0; i < new_len; i++)
+    {
+        if (radiuses[i] > 1)
+        {
+            const char* start = &new_string[i - radiuses[i]];
+            const char* end = &new_string[i + radiuses[i]];
+
+            while (start <= end)
+            {
+                if (*start != '#')
+                {
+                    fputc(*start, stdout);
+                }
+                start++;
+            }
+            printf("\n");
+        }
+    }
+    free(new_string);
+    free(radiuses);
+}
+
 int main()
 {
-    const char* s = "abdabdABBAabdabd";
+    const char* s = "abEFEdabdABBAabdabd";
+    // s = "AA";
+    s = "ABBBBBA";
     solution(s);
+    solution2(s);
 }
 
 static bool is_palindrome(const char* start, const char* end)
