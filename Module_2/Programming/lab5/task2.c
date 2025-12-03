c07564d659ede092de1cc8958803aa7c70c6bd6c
Wool5443 <mihsolodilov2015@gmail.com>
2025-12-03 14:03:58 +0300

    Upd
    Signed-off-by: Wool5443 <mihsolodilov2015@gmail.com>

--- Module_2/Programming/lab5/task2.c
+++ Module_2/Programming/lab5/task2.c
@@ -24,29 +24,9 @@ static void insert_after_odd(Queue* queue, int a1);
 static Node* find_positive(Queue queue);
 static void sort(Node* node);
 
-static void clean_buffer(FILE* stream);
-
-static void read_stdin(const char* save_path);
-static double find_max_neg(const char* path);
-
 int main()
 {
     puts("Лабораторная работа №5");
-
-    puts("Задание №1");
-
-    read_stdin("data.txt");
-
-    double max = find_max_neg("data.txt");
-
-    if (max == -INFINITY)
-    {
-        puts("В последовательности не было отрицательных чисел");
-    }
-    else
-    {
-        printf("max = %lg\n", max);
-    }
 
     puts("Задание №2");
 
@@ -61,7 +41,6 @@ int main()
     puts("Очередь:");
     print_queue(q);
 
-    puts("Задание №3");
     Node* pos = find_positive(q);
     if (!pos)
     {
@@ -205,53 +184,4 @@ static void sort(Node* node)
         }
         n1 = n1->next;
     }
-}
-
-static void read_stdin(const char* save_path)
-{
-    FILE* save = fopen(save_path, "w");
-
-    char buffer[512] = "";
-
-    puts("Введите действительные числа, ввод закончите пустой строкой:");
-
-    fgets(buffer, sizeof(buffer), stdin);
-    while (buffer[1] != '\0')
-    {
-        double n = 0;
-        sscanf(buffer, "%lg", &n);
-        fprintf(save, "%lg\n", n);
-        fgets(buffer, sizeof(buffer), stdin);
-    }
-
-    fclose(save);
-}
-
-static double find_max_neg(const char* path)
-{
-    FILE* f = fopen(path, "r");
-
-    char buffer[512] = "";
-
-    double max = -INFINITY;
-
-    while (fgets(buffer, sizeof(buffer), f))
-    {
-        double n = 0;
-        sscanf(buffer, "%lg", &n);
-
-        if (n < 0 && n > max)
-        {
-            max = n;
-        }
-    }
-
-    fclose(f);
-
-    return max;
-}
-
-static void clean_buffer(FILE* stream)
-{
-    while (fgetc(stream) != '\n');
 }
