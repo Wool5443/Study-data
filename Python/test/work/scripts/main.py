5f485105238bd98f9f1a5b455d69a729359b144f
Misha Solodilov <mihsolodilov2015@gmail.com>
2026-04-22 14:06:45 +0300

    Upd
    Signed-off-by: Misha Solodilov <mihsolodilov2015@gmail.com>

--- Python/test/work/scripts/main.py
+++ Python/test/work/scripts/main.py
@@ -1,8 +1,46 @@
-import matplotlib as plt
-import numpy as np
 import pandas as pd
 
-FF_1 = pd.DataFrame([[1, 2], [3, 4]], columns=["A", "B"], index=["a", "b"])  # pyright: ignore[reportArgumentType]
+
+def input_yn(prompt):
+    print(prompt)
+    while True:
+        res = input()
+        if res in list("yn"):
+            break
+        print("Неправильный ввод, попробуйте ещё раз")
+
+    return res
 
 
-print(FF_1.values)
+data = pd.read_excel("./data/AUTO21053A.xlsx", "data")
+
+
+while True:
+    while True:
+        print("Укажите минимальную и максимальную цену через пробел:")
+        try:
+            min_price, max_price = map(float, input().replace(",", ".").split())
+            break
+        except Exception:
+            print("Неправильный ввод, попробуйте ещё раз")
+
+    def yn_to_est_net(x):
+        return "есть" if x == "y" else "нет"
+
+    need_signal = yn_to_est_net(input_yn("Нужна ли сигнализация? [yn]:"))
+    need_music = yn_to_est_net(input_yn("Нужна ли музыкальная система? [yn]:"))
+
+    selector_price = (data["price"] >= min_price) & (data["price"] <= max_price)
+    selector_need_signal = data["signal"] == need_signal
+    selector_need_music = data["music"] == need_music
+
+    selector = selector_price & selector_need_signal & selector_need_music
+
+    chosen = data.loc[selector, :]
+
+    print(chosen.to_markdown())
+
+    go_on = input_yn("Продолжить? [yn]:")
+
+    if go_on == "n":
+        break
